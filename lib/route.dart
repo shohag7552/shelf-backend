9eb47a20a0dd586a576b50ec910dafcc56fc1513
Mehedi05739 <mehedi@6amtech.com>
2025-06-03 12:37:32 +0600

    first commit

---
+++ lib/route.dart
@@ -0,0 +1,16 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+final router = Router()
+  ..get('/', _rootHandler)
+  ..get('/echo/<message>', _echoHandler);
+
+Response _rootHandler(Request req) {
+  return Response.ok('Hello, World!\n');
+}
+
+Response _echoHandler(Request request) {
+  final message = request.params['message'];
+  return Response.ok('$message\n');
+}
+
